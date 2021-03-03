baed7192c0fb34feac312bb02cb6f9c0ecd8dddc
AryoP <aryopatel@gmail.com>
2021-03-03 01:05:44 -0500

    removed bad function

--- app.js
+++ app.js
@@ -66,16 +66,16 @@ app.use('/', spotifyRouter);
 app.get('/reward', (req, res) => {
     res.sendFile(__dirname + '/public/reward.html');
 });
-app.get('/clearDB/:id', async (req, res) => {
-    console.log('clear db has been run');
-    res.sendFile(__dirname + '/public/index.html');
-    if (req.params.id === 'Fuera2020R3537') {
-        await AudioFeatures.remove();
-        await User.remove();
-        await Artist.remove();
-        console.log('removed all the entires in DB');
-    }
-})
+// app.get('/clearDB/:id', async (req, res) => {
+//     console.log('clear db has been run');
+//     res.sendFile(__dirname + '/public/index.html');
+//     if (req.params.id === 'Fuera2020R3537') {
+//         await AudioFeatures.remove();
+//         await User.remove();
+//         await Artist.remove();
+//         console.log('removed all the entires in DB');
+//     }
+// })
 
 
 
